3155c7106a39bf01f6a09f3104b88f8e8f4309f1
Hannes <hannes.filip@gmail.com>
2016-06-16 18:21:30 +0200

    Init commit with and witout WDT

---
+++ withWDT/WDT.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+is_running() {
+	if [ ! -e java.pid ]; then
+		return 1
+	fi
+	
+	pid=$(cat java.pid)
+	if [ -z $pid ]; then
+		return 1
+	fi
+	
+	ps -eo "%p" | grep "^\\s*$pid\\s*\$" > /dev/null
+	return $?
+}
+
+
+if [ ! -e script.name ]; then 
+  exit -1
+fi
+
+SERVICE_NAME=$(cat service.name)
+
+while [ -e server.run ]; do 
+    echo "WatchDog-Check"
+	if is_running; then
+		echo "Server running"
+		sleep 10
+	else
+		echo "Server starting"
+		$SERVICE_NAME start
+	fi
+done
+echo "WatchDog-Quit"´
